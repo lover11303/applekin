0b3c3e1dca0c6e9c6586e2611306de54c6153474
Apple <Apple@Apple-PC>
2012-06-15 17:44:26 +0800

    version 1.1

--- hihi/src/your/pacage/namespace/HihiActivity.java
+++ hihi/src/your/pacage/namespace/HihiActivity.java
@@ -9,5 +9,6 @@ public class HihiActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        //abcde
     }
 }
